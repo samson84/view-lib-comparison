9b3ce235f39dc33f81f48dc48d134421e17b663f
Daniel Hatas <samson84@gmail.com>
2023-12-05 02:42:22 +0100

    [server] Add morgan logger.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express, { json, Router } from 'express';
 import todos from './todos.router.js'
+import morgan from 'morgan';
 
 const PORT = 4400
 
+const api = Router()
+api.use('/todos', todos)
+
 const app = express();
 app.use(json())
-const api = Router()
+app.use(morgan('dev'))
 app.use('/api', api)
-api.use('/todos', todos)
 
 app.listen(PORT, () => { console.log(`listening on ${PORT}`); })
