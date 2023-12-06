5bb2b51a33d690f0aac7b0c3df0542aa22e09bd9
Daniel Hatas <samson84@gmail.com>
2023-12-06 01:34:59 +0100

    [react-client] improved todo editing ESC exists the todo edit mode autofocus when entering edit mode

--- client-react/src/components/EditTodoItem.tsx
+++ client-react/src/components/EditTodoItem.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
 import { Todo, UpdateTodo } from "../todo-api";
 
 const CancelIcon = () => <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" /></svg>
@@ -14,19 +14,32 @@ export default function EditTodoItem ({
   onCancel
 }: EditTodoItemProps) {
   const [title, setTitle] = useState(todo.title)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef?.current?.focus()
+  }, [])
 
   const handleSave = (event: SyntheticEvent) => {
     event.preventDefault()
     onSave({ title })
   }
 
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      onCancel()
+    }
+  }
+
   return (
     <div className="flex items-center gap-3 h-12 w-80">
     <form onSubmit={handleSave}>
       <input
+        ref={inputRef}
         type="text"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="Todo title"
         className="input input-bordered  w-full max-w-xs"
       />
